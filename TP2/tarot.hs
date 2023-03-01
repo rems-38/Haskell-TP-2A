45f5388c23fd4c05e34eec8c43c1ef79df23909d
rems-38 <rem.mazzone@orange.fr>
2023-03-01 15:25:35 +0100

    tarot cartes_possibles_hors_excuse

--- TP2/tarot.hs
+++ TP2/tarot.hs
@@ -8,6 +8,14 @@ instance Eq Couleur where
     (==) Trèfle Trèfle = True
     (==) _ _ = False
 
+instance Eq Carte where
+    (==) (Standard c1 x1) (Standard c2 x2) = if c1 == c2 && x1 == x2 then True else False
+    (==) (Atout x1) (Atout x2) = if x1 == x2 then True else False
+    (==) Excuse Excuse = True
+    (==) (Standard _ _) _ = False
+    (==) (Atout _) _ = False
+    (==) Excuse _ = False
+
 comparer :: Carte -> Carte -> Bool
 comparer (Standard c1 x) (Standard c2 y) = if c1 == c2 then x >= y else False
 comparer (Atout x) (Atout y) = x >= y
@@ -27,3 +35,12 @@ atout_plus_grand _ _ = False
 atout :: Carte -> Bool
 atout (Atout _) = True
 atout _ = False
+
+(|||) :: (Eq a) => [a] -> [a] -> [a]
+(|||) l1 l2 = if l1 /= [] then l1 else l2
+
+cartes_possibles_hors_excuse :: Carte -> [Carte] -> [Carte]
+cartes_possibles_hors_excuse (Standard c _) cartes = (filter (meme_couleur c) cartes ||| filter atout cartes) ||| cartes
+cartes_possibles_hors_excuse (Atout x) cartes = (filter (atout_plus_grand x) cartes ||| filter atout cartes) ||| cartes
+
+--cartes_possibles :: Carte -> [Carte] -> [Carte]
