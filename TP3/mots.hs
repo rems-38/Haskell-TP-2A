5d4e0c6df27abaa14a7624e2e0ace19428f0d112
rems-38 <rem.mazzone@orange.fr>
2023-03-01 17:38:50 +0100

    tp3 mots

--- TP3/mots.hs
+++ TP3/mots.hs
@@ -1,5 +1,5 @@
 espace :: Char -> Bool
-espace x = if x == ' ' then True else False
+espace x = x == ' '
 
 left :: (a, b) -> a 
 left (x, _) = x
@@ -7,9 +7,19 @@ right :: (a, b) -> b
 right (_, x) = x
 
 separe :: (Char -> Bool) -> String -> (String, String)
-separe op [] = ([], [])
-separe op l@(x:xs)
-    | op x = (x:(left reste), right reste) 
-    | otherwise = ([], l)
-    where
-        reste = separe op xs
+separe op s = (left separation, dropWhile op (right separation))
+    where separation = span (not . op) s
+
+grignote_espaces :: String -> String
+grignote_espaces [] = ""
+grignote_espaces (x:xs)
+    | espace x = grignote_espaces xs
+    | otherwise = x:xs
+
+un_mot :: String -> (String, String)
+un_mot mot = (left function, grignote_espaces (right function))
+    where function = separe espace (grignote_espaces mot)
+
+mots :: String -> [String]
+mots [] = []
+mots mot = (left (un_mot mot)) : mots (right (un_mot mot))
