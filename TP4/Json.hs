322441b13529faadc72a33225a33d2ce1a1eeb9f
rems-38 <rem.mazzone@orange.fr>
2023-03-22 20:13:24 +0100

    show (JSON tp4)

--- TP4/Json.hs
+++ TP4/Json.hs
@@ -1,3 +1,5 @@
+import Data.List (intercalate)
+
 data JSON =
     JSON_Int Integer |
     JSON_Bool Bool |
@@ -10,3 +12,21 @@ exemple_tableau = JSON_Array [JSON_Int 42, JSON_Bool True, JSON_String "JSON!", 
 
 exemple_objet :: JSON
 exemple_objet = JSON_Object [("entier", JSON_Int 42), ("texte", JSON_String "Haskell!"), ("tableau", JSON_Array [JSON_Bool True])]
+
+show_json :: JSON -> String
+show_json (JSON_Int n) = show n
+show_json (JSON_Bool b) = show b
+show_json (JSON_String s) = show s
+show_json (JSON_Array []) = "[]"
+show_json (JSON_Array xs) = "[" ++ (intercalate ", " (map show_json xs)) ++ "]"
+show_json (JSON_Object []) = "{}"
+show_json (JSON_Object xs) = "{" ++ (intercalate ", " (map show_pair xs)) ++ "}"
+  where
+    show_pair (k, v) = show k ++ ": " ++ show_json v
+
+instance Show JSON where
+    show = show_json
+
+-- Bonus/personal function to get "hello" and not "\"hello\""
+print_json :: JSON -> IO()
+print_json json = putStrLn (show_json json)
