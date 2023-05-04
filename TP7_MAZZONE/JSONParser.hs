f52fdf0b37ffee8ead602b9ff492b06eb81f7cbe
rems-38 <rem.mazzone@orange.fr>
2023-05-04 15:01:20 +0200

    TP7 JSON next

--- TP7_MAZZONE/JSONParser.hs
+++ TP7_MAZZONE/JSONParser.hs
@@ -1,8 +1,9 @@
 import Parser
+import Data.Maybe
 
 pTabChar :: [Char] -> Parser ()
 pTabChar [] str = Just ((), str)
-pTabChar (c:cr) str = (pCaractere c >>> (pTabChar cr)) str
+pTabChar (c:cr) str = (pCaractere c >>> pTabChar cr) str
 
 getReste :: Maybe (a, String) -> String
 getReste Nothing = ""
@@ -10,16 +11,51 @@ getReste (Just (_, str)) = str
 
 pBooleen :: Parser Bool
 pBooleen str = case sauterEspaces str of
-                't' : r -> if (pTabChar ['r', 'u', 'e'] r) /= Nothing
+                't' : r -> if isJust (pTabChar ['r', 'u', 'e'] r)
                             then Just (True, getReste (pTabChar ['r', 'u', 'e'] r))
                             else Nothing
-                'f' : r -> if (pTabChar ['a', 'l', 's', 'e'] r) /= Nothing
+                'f' : r -> if isJust (pTabChar ['a', 'l', 's', 'e'] r)
                             then Just (False, getReste (pTabChar ['a', 'l', 's', 'e'] r))
                             else Nothing
 
 -- Combinateur "alternative"
-(<||>) :: (Eq a) => Parser a -> Parser a -> Parser a
-(<||>) p1 p2 str | p1 str /= Nothing = p1 str
+(<||>) :: Eq a => Parser a -> Parser a -> Parser a
+(<||>) p1 p2 str | isJust (p1 str) = p1 str
                  | otherwise = p2 str
 
--- cRepeterVirgules :: Parser a -> Parser [a]
+cRepeterVirgules :: Eq a => Parser a -> Parser [a]
+cRepeterVirgules p = cRepeter (pCaractere ',' >>> p <||> p)
+
+data JSON =
+    JSON_Int Integer |
+    JSON_Bool Bool |
+    JSON_String String |
+    JSON_Array [JSON] |
+    JSON_Object [(String, JSON)] deriving (Eq, Show)
+
+pNombreJSON :: Parser JSON
+pNombreJSON = JSON_Int <$$> pNombre
+
+pBooleenJSON :: Parser JSON
+pBooleenJSON = JSON_Bool <$$> pBooleen
+
+pChaineJSON :: Parser JSON
+pChaineJSON = JSON_String <$$> pChaine
+
+(<<<) :: Parser b -> Parser a -> Parser b
+p1 <<< p2 = p2 >>> p1
+
+pTableauJSON :: Parser JSON
+pTableauJSON = JSON_Array <$$> (pCaractere '[' >>> cRepeterVirgules pJSON <<< pCaractere ']')
+
+-- pAttributJSON :: Parser (String, JSON)
+-- pAttributJSON = (,) <$$> pChaine <<< pCaractere ':' <*> pJSON
+
+-- pObjetJSON :: Parser JSON
+-- pObjetJSON = JSON_Object <$$> (pCaractere '{' >>> cRepeterVirgules pAttributJSON <<< pCaractere '}')
+
+pJSON :: Parser JSON
+pJSON = pNombreJSON <||> pBooleenJSON <||> pChaineJSON <||> pTableauJSON -- <||> pObjetJSON
+
+unMaybe :: Maybe a -> a
+unMaybe (Just a) = a
